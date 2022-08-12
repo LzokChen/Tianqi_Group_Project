68a66fcbf44f8384e35868de84d3ee37127f91c7
Mingyu Liu <mingyuliu88@gmail.com>
2022-08-12 14:44:29 -0400

    Fetch data for WeatherDetailView

--- ClimaWeather/Views/WeatherDetailView.swift
+++ ClimaWeather/Views/WeatherDetailView.swift
@@ -7,40 +7,102 @@
 
 import SwiftUI
 
-struct WeatherDetailView: View {
+struct WeatherDetail:Hashable {
+    let city : String
+    let temperature: String
+    let pressure: String
+    let humidity: String
+    let windSpeed: String
+    let ultravioletIndex: String
+    let sunRise: String
+    let visibility: String
+    let sunSet: String
+    let realFeel: String
+    let realFeelTip: String
+}
+
+struct WeatherDetailView: View, WeatherManagerDelegate {
+    @State private var weatherDetail: WeatherDetail? = nil
+    @State private var hasUpdatedWeatherDetail = false
+    
+    func didUpdateCurrentWeather(_ weatherManager: WeatherManager, weather: CurrentWeatherModel) {
+        let uvi = (Int)(weather.uvi) ?? 0
+        let uviDescription = (uvi <= 2 ? "最弱" : uvi <= 4 ? "弱" : uvi <= 6 ? "中等" : uvi <= 9 ? "强" : "很强")
+        
+        var visibility = (Double)(weather.vis) ?? 0
+        visibility = round(visibility / 100) / 10.0
+        let visibilityDescription = (String)(visibility) + "公里"
+        
+        let pressure = (Double)(weather.pressure) ?? 0
+        let pressureDescription = String(Int(round(pressure / 10))) + "kPa"
+        
+        weatherDetail = WeatherDetail(city: weather.secondaryName,
+                                      temperature: weather.temperature + "º | " + weather.condition,
+                                      pressure: pressureDescription,
+                                      humidity: weather.humidity + "%",
+                                      windSpeed: weather.windSpeed + "m/s",
+                                      ultravioletIndex: weather.uvi + " " + uviDescription,
+                                      sunRise: String(weather.sunRise.components(separatedBy: " ")[1].prefix(5)),
+                                      visibility: visibilityDescription,
+                                      sunSet: String(weather.sunSet.components(separatedBy: " ")[1].prefix(5)),
+                                      realFeel: weather.realFeel + "º",
+                                      realFeelTip: weather.tips
+        )
+        hasUpdatedWeatherDetail = true
+        print(weatherDetail ?? "")
+    }
+    
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false){
+        //MARK: - Usage: create weatherManager and setup the delegate
+        let wm = WeatherManager()
+        let _ = wm.setDelegate(delegate: self)
+        
+        if (!hasUpdatedWeatherDetail) {
+            //let _ = wm.fetchCurrentWeather(latitude: 22.555259, longitude: 113.88402)
+            let _ = wm.fetchCurrentWeather(address: "上海")
+        }
+        
+        if (!hasUpdatedWeatherDetail) {
             VStack{
-                // City Weather
+                LottieView(lottieFile: "load.json")
+                    .frame(width: 300, height: 300)
+                Text("Fetching the weather details...")
+                    .frame(alignment: .center)
+            }
+        } else {
+            ScrollView(.vertical, showsIndicators: false){
                 VStack{
-                    Text("深圳市")
-                        .font(.system(size: 35, weight: .bold, design: .monospaced))
-                    Text("25º | 局部多云")
-                        .fontWeight(.bold)
-                }
-                Spacer().frame(height: 35)
-                VStack{
-                    HStack{
-                        DetailCard(icon: "sun.max.fill",detailText: "紫外线指数", detailDescripe: "0 低", singleDescripe:"")
-                        DetailCard(icon: "sunrise",detailText: "日出", detailDescripe: "05:59", singleDescripe:"日落: 18:59")
+                    // City Weather
+                    VStack{
+                        Text(weatherDetail?.city ?? "Unknown City")
+                            .font(.system(size: 35, weight: .bold, design: .monospaced))
+                        Text(weatherDetail?.temperature ?? " ")
+                            .fontWeight(.bold)
                     }
-                }
-                VStack{
-                    HStack{
-                        DetailCard(icon: "wind",detailText: "风速", detailDescripe: "12m/s", singleDescripe:"")
-                        DetailCard(icon: "cloud.drizzle",detailText: "降雨", detailDescripe: "1 毫米", singleDescripe:"预计未来24小时内有23毫米")
+                    Spacer().frame(height: 35)
+                    VStack{
+                        HStack{
+                            DetailCard(icon: "sun.max.fill",detailText: "紫外线指数", detailDescripe: (weatherDetail?.ultravioletIndex ?? "N/A"), singleDescripe:"")
+                            DetailCard(icon: "sunrise",detailText: "日出", detailDescripe: (weatherDetail?.sunRise ?? "N/A"), singleDescripe:"日落: " + (weatherDetail?.sunSet ?? "N/A"))
+                        }
                     }
-                }
-                VStack{
-                    HStack{
-                        DetailCard(icon: "thermometer.low",detailText: "体感温度", detailDescripe: "30ºC", singleDescripe:"潮湿使人感觉更暖和")
-                        DetailCard(icon: "humidity.fill",detailText: "湿度", detailDescripe: "93%", singleDescripe:"目前露点温度为24º")
+                    VStack{
+                        HStack{
+                            DetailCard(icon: "wind",detailText: "风速", detailDescripe: (weatherDetail?.windSpeed ?? "N/A"), singleDescripe:"")
+                            DetailCard(icon: "cloud.drizzle",detailText: "降雨", detailDescripe: "1 毫米", singleDescripe:"预计未来24小时内有23毫米")
+                        }
                     }
-                }
-                VStack{
-                    HStack{
-                        DetailCard(icon: "eye",detailText: "能见度", detailDescripe: "8 公里", singleDescripe:"薄雾影响了能见度")
-                        DetailCard(icon: "cloud.bolt.circle",detailText: "气压", detailDescripe: "1kPa", singleDescripe:"")
+                    VStack{
+                        HStack{
+                            DetailCard(icon: "thermometer.low", detailText: "体感温度", detailDescripe: (weatherDetail?.realFeel ?? "N/A"), singleDescripe: (weatherDetail?.realFeelTip ?? "N/A"))
+                            DetailCard(icon: "humidity.fill", detailText: "湿度", detailDescripe: (weatherDetail?.humidity ?? "N/A"), singleDescripe: "")
+                        }
+                    }
+                    VStack{
+                        HStack{
+                            DetailCard(icon: "eye", detailText: "能见度", detailDescripe: (weatherDetail?.visibility ?? "N/A"), singleDescripe: "")
+                            DetailCard(icon: "cloud.bolt.circle", detailText: "气压", detailDescripe: (weatherDetail?.pressure ?? "N/A"), singleDescripe: "")
+                        }
                     }
                 }
             }
@@ -55,13 +117,10 @@ struct WeatherDetail_Previews: PreviewProvider {
 }
 
 struct DetailCard: View {
-    
     let icon:String
     let detailText:String
     let detailDescripe:String
     let singleDescripe:String
-    
-    
     
     var body: some View {
         HStack {
@@ -72,7 +131,7 @@ struct DetailCard: View {
                 }
                 Spacer().frame(height: 10)
                 Text(detailDescripe)
-                    .font(.system(size: 35, weight: .heavy))
+                    .font(.system(size: 28, weight: .heavy))
                 Spacer()
                 Text(singleDescripe)
             }
@@ -82,8 +141,5 @@ struct DetailCard: View {
         .frame(width: UIScreen.main.bounds.width/2*0.80, height: UIScreen.main.bounds.width/2*0.80)
         .background(Color.secondary.opacity(0.3).cornerRadius(15))
         .foregroundColor(.secondary)
-        
     }
-    
-    
 }
