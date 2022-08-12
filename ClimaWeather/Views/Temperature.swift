df29783ac76b27b8c79b2ea735869ba11340e840
Mingyu Liu <mingyuliu88@gmail.com>
2022-08-11 21:06:03 -0400

    Fetch current weather details dynamically

--- ClimaWeather/Views/Temperature.swift
+++ ClimaWeather/Views/Temperature.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct CurrentWeather:Hashable {
+    let city : String
+    let time : String
+    let temperature: String
+    let description: String
+    let pressure: String
+    let humidity: String
+    let windSpeed: String
+}
+
 struct DailyWeather:Hashable {
     let day : String
     let date : String
@@ -21,6 +31,9 @@ struct HourlyWeather:Hashable {
 }
 
 struct Temperature: View, WeatherManagerDelegate {
+    @State private var currentWeather: CurrentWeather? = nil
+    @State private var hasUpdatedCurrentWeather = false
+    
     @State private var dailyWeathers = [DailyWeather]()
     @State private var hasUpdatedDailyWeather = false
     
@@ -28,23 +41,36 @@ struct Temperature: View, WeatherManagerDelegate {
     @State private var hasUpdatedHourlyWeather = false
     
     /*
-    let dailyWeahter : [DailyWeather] = [
-        DailyWeather(day: "8月12日", date: "明日", temperature: "23º", icon: "sun.min"),
-        DailyWeather(day: "8月13日", date: "星期一", temperature: "23º", icon: "sun.min.fill"),
-        DailyWeather(day: "8月14日", date: "星期二", temperature: "23º", icon: "sun.max"),
-        DailyWeather(day: "8月15日", date: "星期三", temperature: "23º", icon: "sun.max.fill"),
-        DailyWeather(day: "8月16日", date: "星期四", temperature: "23º", icon: "cloud.drizzle"),
-        DailyWeather(day: "8月17日", date: "星期五", temperature: "23º", icon: "cloud.drizzle.fill"),
-        DailyWeather(day: "8月18日", date: "星期六", temperature: "23º", icon: "cloud.rain"),
-        DailyWeather(day: "8月19日", date: "星期日", temperature: "23º", icon: "cloud.rain.fill"),
-        DailyWeather(day: "8月20日", date: "星期一", temperature: "23º", icon: "cloud.snow"),
-        DailyWeather(day: "8月21日", date: "星期二", temperature: "23º", icon: "cloud.snow.fill")
-    ]*/
+     let dailyWeahter : [DailyWeather] = [
+     DailyWeather(day: "8月12日", date: "明日", temperature: "23º", icon: "sun.min"),
+     DailyWeather(day: "8月13日", date: "星期一", temperature: "23º", icon: "sun.min.fill"),
+     DailyWeather(day: "8月14日", date: "星期二", temperature: "23º", icon: "sun.max"),
+     DailyWeather(day: "8月15日", date: "星期三", temperature: "23º", icon: "sun.max.fill"),
+     DailyWeather(day: "8月16日", date: "星期四", temperature: "23º", icon: "cloud.drizzle"),
+     DailyWeather(day: "8月17日", date: "星期五", temperature: "23º", icon: "cloud.drizzle.fill"),
+     DailyWeather(day: "8月18日", date: "星期六", temperature: "23º", icon: "cloud.rain"),
+     DailyWeather(day: "8月19日", date: "星期日", temperature: "23º", icon: "cloud.rain.fill"),
+     DailyWeather(day: "8月20日", date: "星期一", temperature: "23º", icon: "cloud.snow"),
+     DailyWeather(day: "8月21日", date: "星期二", temperature: "23º", icon: "cloud.snow.fill")
+     ]*/
     
     //MARK: - WeatherManagerDelegate functions
     func didUpdateCurrentWeather(_ weatherManager: WeatherManager, weather: CurrentWeatherModel) {
-        print(weather.pname, weather.secondaryName, weather.name, weather.temperature, weather.sunRise, weather.sunSet)
-        print(weather.tips)
+        // Get the current date and time
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+        
+        currentWeather = CurrentWeather(city: weather.secondaryName,
+                                        time: formatter.string(from: currentDateTime),
+                                        temperature: weather.temperature,
+                                        description: weather.condition,
+                                        pressure: weather.pressure + "hpa",
+                                        humidity: weather.humidity + "%",
+                                        windSpeed: weather.windSpeed + "km/h")
+        hasUpdatedCurrentWeather = true
+        print(currentWeather ?? "")
     }
     
     func didUpdate24HoursForecast(_ weatherManager: WeatherManager, forecasts: [HourlyForecastModel]) {
@@ -84,9 +110,12 @@ struct Temperature: View, WeatherManagerDelegate {
         let wm = WeatherManager()
         let _ = print(wm.delegate = self)
         
-        if (!hasUpdatedDailyWeather) {
+        if (!hasUpdatedCurrentWeather) {
             //let _ = wm.fetchCurrentWeather(latitude: 22.555259, longitude: 113.88402)
-            //let _ = wm.fetchCurrentWeather(address: "上海")
+            let _ = wm.fetchCurrentWeather(address: "上海")
+        }
+        
+        if (!hasUpdatedDailyWeather) {
             let _ = wm.fetch15DaysForecast(address: "上海")
         }
         
@@ -94,107 +123,112 @@ struct Temperature: View, WeatherManagerDelegate {
             let _ = wm.fetch24HoursForecast(address: "上海")
         }
         
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack{
-                // Weather Card
-                VStack(alignment: .center){
-                    // Location and time
-                    HStack{
-                        Image(systemName: "location.fill")
-                        Text("深圳市")
-                            .fontWeight(.bold)
-                        Spacer()
-                        Text("08:52 上午")
-                    }
-                    
-                    Spacer().frame(height: 35)
-                    
-                    // Temperature
-                    HStack(alignment: .top, spacing: 0){
-                        Text("22")
-                            .font(.system(size: 95, weight: .bold, design: .monospaced))
-                        Text("º")
-                            .font(.system(size: 30, weight: .bold, design: .monospaced))
-                    }
-                    
-                    Spacer().frame(height: 2)
-                    // Single description
-                    Text("局部多云")
-                    
-                    Spacer().frame(height: 15)
-                    
-                    // Presure Huminity Wind
-                    HStack{
-                        HStack{
-                            Image(systemName: "aqi.medium")
-                            Text("720hpa")
-                        }
-                        Spacer()
-                        HStack{
-                            Image(systemName: "humidity")
-                            Text("32%")
-                        }
-                        Spacer()
-                        HStack{
-                            Image(systemName: "wind")
-                            Text("12km/h")
-                        }
-                    }
-                    
-                }
-                .padding()
-                .foregroundColor(Color.white)
-                .background(Color.secondary.cornerRadius(10))
-                
-                Divider()
-                
-                // Today's Weather: Show hourly weather and temperature
-                // Horizontally Scrollable Component
-                ScrollView(.horizontal, showsIndicators: false){
-                    HStack(spacing: 12){
-                        ForEach(hourlyWeathers, id: \.self){ weather in
-                            VStack{
-                                Text(weather.hour)
-                                    .font(.system(size: 14, weight: .bold))
-                                Spacer().frame(height: 5)
-                                Image(weather.icon)
-                                    .renderingMode(.template)
-                                    .frame(width: 60, height: 60)
-                                    .foregroundColor(.secondary)
-                                    .scaledToFill()
-                                Spacer().frame(height: 7)
-                                Text(weather.temperature + "º")
-                            }
-                        }
-                    }
-                }
-                
-                Divider()
-                
-                // Future daily weather
+        if (hasUpdatedCurrentWeather && hasUpdatedHourlyWeather && hasUpdatedDailyWeather) {
+            ScrollView(.vertical, showsIndicators: false) {
                 VStack{
-                    ForEach(dailyWeathers, id:\.self){ weather in
+                    // Weather Card
+                    VStack(alignment: .center){
+                        // Location and time
+                        HStack{
+                            Image(systemName: "location.fill")
+                            Text(currentWeather?.city ?? "Unknown City")
+                                .fontWeight(.bold)
+                            Spacer()
+                            Text(currentWeather?.time ?? "Unknown Time")
+                        }
+                        
+                        Spacer().frame(height: 35)
+                        
+                        // Temperature
+                        HStack(alignment: .top, spacing: 0){
+                            Text(currentWeather?.temperature ?? "Unknown Temperature")
+                                .font(.system(size: 95, weight: .bold, design: .monospaced))
+                            Text("º")
+                                .font(.system(size: 30, weight: .bold, design: .monospaced))
+                        }
+                        
+                        Spacer().frame(height: 2)
+                        // Single description
+                        Text(currentWeather?.description ?? "")
+                        
+                        Spacer().frame(height: 15)
+                        
+                        // Presure Huminity Wind
                         HStack{
                             HStack{
-                                VStack(alignment: .leading){
-                                    Text(weather.date)
-                                        .font(.system(size: 19, weight: .bold))
-                                    Spacer().frame(height: 5)
-                                    Text(weather.day)
-                                        .font(.subheadline)
-                                }
-                                Spacer()
+                                Image(systemName: "aqi.medium")
+                                Text(currentWeather?.pressure ?? "N/A")
                             }
-                            .frame(width: 100)
                             Spacer()
-                            Text(weather.temperature)
+                            HStack{
+                                Image(systemName: "humidity")
+                                Text(currentWeather?.humidity ?? "N/A")
+                            }
                             Spacer()
-                            Image(weather.icon)
-                        }.padding()
+                            HStack{
+                                Image(systemName: "wind")
+                                Text(currentWeather?.windSpeed ?? "N/A")
+                            }
+                        }
+                        
+                    }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.secondary.cornerRadius(10))
+                    
+                    Divider()
+                    
+                    // Today's Weather: Show hourly weather and temperature
+                    // Horizontally Scrollable Component
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: 12){
+                            ForEach(hourlyWeathers, id: \.self){ weather in
+                                VStack{
+                                    Text(weather.hour)
+                                        .font(.system(size: 14, weight: .bold))
+                                    Spacer().frame(height: 5)
+                                    Image(weather.icon)
+                                        .renderingMode(.template)
+                                        .frame(width: 60, height: 60)
+                                        .foregroundColor(.secondary)
+                                        .scaledToFill()
+                                    Spacer().frame(height: 7)
+                                    Text(weather.temperature + "º")
+                                }
+                            }
+                        }
+                    }
+                    
+                    Divider()
+                    
+                    // Future daily weather
+                    VStack{
+                        ForEach(dailyWeathers, id:\.self){ weather in
+                            HStack{
+                                HStack{
+                                    VStack(alignment: .leading){
+                                        Text(weather.date)
+                                            .font(.system(size: 19, weight: .bold))
+                                        Spacer().frame(height: 5)
+                                        Text(weather.day)
+                                            .font(.subheadline)
+                                    }
+                                    Spacer()
+                                }
+                                .frame(width: 100)
+                                Spacer()
+                                Text(weather.temperature)
+                                Spacer()
+                                Image(weather.icon)
+                            }.padding()
+                        }
                     }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+        } else {
+            Text("Fetching the weather details...")
+                .frame(maxWidth: .infinity, alignment: .center)
         }
     }
 }
