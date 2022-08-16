afcc8d30c0b27203f512529188545b178d5f195c
Xiaojian Chen <lzokchen@ucla.edu>
2022-08-15 20:43:48 -0700

    Tetris Init

---
+++ Tetris/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tetris
+//
+//  Created by Xiaojian Chen on 8/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
