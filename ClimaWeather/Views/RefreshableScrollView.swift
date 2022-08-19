699447048e019cfda3a2500db75c77b556aecfd8
Mingyu Liu <mingyuliu88@gmail.com>
2022-08-18 21:04:47 -0700

    Fix tab bar UI bug

--- ClimaWeather/Views/RefreshableScrollView.swift
+++ ClimaWeather/Views/RefreshableScrollView.swift
@@ -12,7 +12,7 @@ public struct RefreshableScrollView<Content: View>: View {
     var content: Content
     var onRefresh: () -> Void
     @Environment(\.colorScheme) var colorScheme
-
+    
     public init(content: @escaping () -> Content, onRefresh: @escaping () -> Void) {
         self.content = content()
         self.onRefresh = onRefresh
@@ -25,7 +25,7 @@ public struct RefreshableScrollView<Content: View>: View {
             return "https://images.unsplash.com/photo-1522441815192-d9f04eb0615c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
         }
     }
-
+    
     public var body: some View {
         List {
             content
@@ -38,19 +38,16 @@ public struct RefreshableScrollView<Content: View>: View {
             onRefresh()
         }
         .background(WebImage(url: URL(string: imageURL))
-
-                    .onSuccess { image, data, cacheType in
-                    }
-                    .resizable()
-                    .placeholder(Image(systemName: "photo"))
-                    .placeholder {
-                        Rectangle().foregroundColor(.gray)
-                    }
-                    .indicator(.activity) // Activity Indicator
-                    .transition(.fade(duration: 0.5)) // Fade Transition with duration
-                    .scaledToFill()
-//                            .frame(width: 300, height: 300, alignment: .center))
-                    .edgesIgnoringSafeArea(.all))
+                    
+            .onSuccess { image, data, cacheType in
+            }
+            .resizable()
+            .placeholder(Image(systemName: "photo"))
+            .placeholder {
+                Rectangle().foregroundColor(.gray)
+            }
+            .indicator(.activity) // Activity Indicator
+            .transition(.fade(duration: 0.5)) // Fade Transition with duration
+            .scaledToFill())
     }
-    
 }
